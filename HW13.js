13a725038ba607ed23540fcee438fcaf12f00a1f
AaryanshKhera <96333404+AaryanshKhera@users.noreply.github.com>
2021-12-24 09:23:32 +0530

    Add files via upload

---
+++ HW13.js
@@ -0,0 +1,15 @@
+function redballoon() {
+  var red = createSprite
+  Math.round(random(20,370)),10,20
+  red.AddImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+ }
+ // red balloonImage = loadImage("red balloon0.png");
+ // red_balloonImage = loadImage("red_balloon0")
+ // red_balloonImage = loadImage("red_ballon0.png")
+  
+  
+
+
